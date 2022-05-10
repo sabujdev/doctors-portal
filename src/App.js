272779c671d06618d4ce21e818108c61ef9fa7fa
sabujmahonto <sabujmahonto@gmail.com>
2022-05-10 17:57:27 +0600

    Navbar or Banner section added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
+import Home from './Pages/Home/Home';
+import Reviews from './Pages/Reviews/Reviews';
+import Navbar from './Pages/Shared/Navbar/Navbar';
+import Contact from './Pages/Contact/Contact'
+import Login from './Pages/Login/Login'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+   <Navbar></Navbar>
+   <Routes>
+     <Route path='/' element={<Home></Home>}></Route>
+     <Route path='/about' element={<About></About>}></Route>
+     <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+     <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+     <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+     <Route path='/contact' element={<Contact></Contact>}></Route>
+     <Route path='/login' element={<Login></Login>}></Route>
+     
+     
+   </Routes>
     </div>
   );
 }
